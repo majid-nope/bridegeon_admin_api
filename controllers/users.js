c2c16ee839e50d064ad83e6d76087463db110911
majid-nope <muhammed0000majid@gmail.com>
2023-03-16 16:00:08 +0530

    new:add user

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,77 @@
+const { bcryptTool } = require("../lib/bcrypt");
+const courses = require("../models/courses");
 const users = require("../models/users");
 
 const user = {
+  add(req, res) {
+    const user = req.body;
+    // const fields = {
+    //   email: "Email",
+    //   password: "Password",
+    //   phone: "Phone",
+    //   name: "Your name",
+    //   batch: "Batch",
+    // };
+    req.body.forEach((el) => {
+      switch (el.title) {
+        case fields.name:
+          user.name = el.value;
+          break;
+
+        case fields.password:
+          user.password = el.value;
+          break;
+        case fields.email:
+          user.email = el.value;
+          break;
+        case fields.phone:
+          user.phone = el.value;
+          break;
+        case fields.batch:
+          user.batch = el.value;
+        default:
+          break;
+      }
+    });
+    console.log(user);
+    if (user.password) {
+      bcryptTool.GenHash(user.password, 10).then((hashedPassword) => {
+        user.password = hashedPassword;
+
+        courses.findOne({ title: "Web Development" }).then((course) => {
+          course.chapter[0].topics[0].status = "pending";
+
+          course.chapter[0].status = "pending";
+          console.log(course.chapter[0].topics);
+          const Chapters = course.chapter;
+
+          console.log(course);
+          userModel
+            .create({
+              ...user,
+              progress: [
+                {
+                  course: { id: course._id, title: course.title },
+                  chapter: [...Chapters],
+                },
+              ],
+            })
+            .then((el) => {
+              res.status(200).json({ data: true });
+            })
+            .catch((err) => {
+              res.status(400).json({
+                data: err,
+              });
+            });
+        });
+      });
+    } else {
+      res.status(400).json({
+        data: "Some field are missing ",
+      });
+    }
+  },
   getAll(req, res) {
     console.log(req.query);
     const limit = Number(req.query.limit);
